44588acdd44b3f3ddbaba42cc4e0d9432306ef31
wonAdam <>
2021-01-25 23:44:28 +0900

    Pathfinder를 구현하기 위한 밑작업

---
+++ Assets/Scripts/PathFinder/PsuedoCube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PsuedoCube : MonoBehaviour
+{
+    [SerializeField] protected List<PsuedoCube> neighbors = new List<PsuedoCube>();
+    public List<PsuedoCube> Neighbors { get => neighbors; set => neighbors = value; }
+
+    [SerializeField] public Transform platform;
+    public float groundHeight;
+
+    private void Start()
+    {
+        groundHeight = transform.position.y;
+        PsuedoCube[] cubeArr = FindObjectsOfType<PsuedoCube>();
+        foreach(var c in cubeArr)
+        {
+            c.RegisterIfItsNeighbor(this);
+        }
+    }
+
+    public void RegisterIfItsNeighbor(PsuedoCube registerer)
+    {
+        Vector2 registererPlanePos = new Vector2(registerer.transform.position.x, registerer.transform.position.z);
+        Vector2 myPlanePos = new Vector2(transform.position.x, transform.position.z);
+
+        if (Vector2.Distance(registererPlanePos, myPlanePos) < 1.1f && registerer != this)
+        {
+            Neighbors.Add(registerer);
+        }
+    }
+}
---
+++ Assets/Scripts/PathFinder/PsuedoMapMgr.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PsuedoMapMgr : MonoBehaviour
+{
+    [SerializeField] List<PsuedoCube> cubes;
+
+    public List<List<PsuedoCube>> cubeSorted;
+
+    public Map map;
+
+    private void Start()
+    {
+        
+    }
+}
