3f12f6eaa6f3b04e0c8bcb3104dd901cece9499c
wonAdam <ehdgus5500@gmail.com>
2021-01-27 22:44:46 +0900

    path들이 scene window에서 보이게 OnDrawGizmos 작성

--- Assets/Scripts/MapMgr.cs
+++ Assets/Scripts/MapMgr.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 
 public class MapMgr : MonoBehaviour
 {
+    [SerializeField] private bool showPaths;
     public Map map;
+    
     private void Reset()
     {
         map = new Map(FindObjectsOfType<Cube>().ToList());
@@ -29,4 +31,18 @@ public class MapMgr : MonoBehaviour
     }
 
     private void InitializeMapData() => map = new Map(FindObjectsOfType<Cube>().ToList());
+    private void OnDrawGizmos()
+    {
+        if (!showPaths) return;
+
+        List<Cube> cubes = FindObjectsOfType<Cube>().ToList();
+        Vector3 slightlyUp = new Vector3(0f, 0.2f, 0f);
+        foreach (var cube in cubes)
+        {
+            foreach(var neighbor in cube.neighbors)
+            {
+                Debug.DrawLine(cube.platform.position + slightlyUp, neighbor.platform.position + slightlyUp, Color.yellow);
+            }
+        }
+    }
 }
