eaeeefafc2f69ac488d297c37d22b60ea766f3f9
wonAdam <ehdgus5500@gmail.com>
2021-01-28 17:44:20 +0900

    FSM 뼈대 인터페이스들 작성

---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateMachine<T>
+{
+    public T owner;
+
+    public enum StateChangeMethod { PopNPush, JustPush, ReturnToPrev }
+
+    public Stack<IState<T>> stateStack;
+
+    public virtual void Run()
+    {
+        stateStack.Peek().Execute();
+    }
+
+    public virtual void ChangeState(IState<T> nextState, StateChangeMethod method)
+    {
+        switch (method)
+        {
+            case StateChangeMethod.PopNPush:
+                {
+                    IState<T> prevState = stateStack.Pop();
+                    prevState.Exit();
+                    stateStack.Push(nextState);
+                    nextState.Enter();
+                    break;
+                }
+                
+            case StateChangeMethod.JustPush:
+                {
+                    IState<T> prevState = stateStack.Peek();
+                    prevState.Exit();
+                    stateStack.Push(nextState);
+                    nextState.Enter();
+                    break;
+                }
+
+            case StateChangeMethod.ReturnToPrev:
+                {
+                    IState<T> currState = stateStack.Pop();
+                    currState.Exit();
+                    IState<T> prevState = stateStack.Peek();
+                    prevState.Enter();
+                    break;
+                }
+        }
+
+    }
+
+}
