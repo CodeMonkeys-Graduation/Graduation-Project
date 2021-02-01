684a80cac6b27238f59e652477672cddf697f144
ParkSolHwi <>
2021-02-01 23:37:24 +0900

    CameraMove

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    private static CameraMove instance;
+    public static CameraMove Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<CameraMove>();
+            }
+            return instance;
+        }
+        private set
+        {
+            instance = value;
+        }
+    }
+    public enum CameraState
+    {
+        NonTarget,
+        UnitMove,
+        UnitChange
+    }
+    public Vector3 offset;
+    CameraState cameraState;
+    Transform target;
+    void Awake()
+    {
+        if (Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        cameraState = CameraState.NonTarget;
+    }
+    void FixedUpdate()
+    {
+        if (cameraState == CameraState.NonTarget)
+        {
+            return;
+        }
+        else if (cameraState == CameraState.UnitMove)
+        {
+            transform.position = target.position + offset;
+        }
+        else if (cameraState == CameraState.UnitChange)
+        {
+            Vector3 unitPos = new Vector3(target.position.x, target.position.y, target.position.z);
+            unitPos += offset;
+            transform.position = Vector3.Lerp(transform.position, unitPos, Time.deltaTime * 2f);
+
+            if (Mathf.Abs(transform.position.y - unitPos.y) <= Mathf.Epsilon)
+            {
+                cameraState = CameraState.UnitMove;
+                Debug.Log("Camera Move Completed");
+            }
+        }
+    }
+    public void ChangeTarget(Unit unit)
+    {
+        cameraState = CameraState.UnitChange;
+        target = unit.transform;
+    }
+}
--- Assets/Scripts/PathFinder/Test/UnitClickTest.cs
+++ Assets/Scripts/PathFinder/Test/UnitClickTest.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class UnitClickTest : MonoBehaviour
 {
-    [Header ("Reset Before Test")]
+    [Header("Reset Before Test")]
     [SerializeField] public Unit selectedUnit;
     [SerializeField] public MapMgr mapMgr;
     private void Reset()
@@ -14,7 +14,7 @@ public class UnitClickTest : MonoBehaviour
     }
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -22,11 +22,12 @@ public class UnitClickTest : MonoBehaviour
             {
                 hit.transform.GetComponent<IClickable>()?.OnClick();
                 selectedUnit = hit.transform.GetComponent<Unit>();
+                CameraMove.Instance.ChangeTarget(selectedUnit);
                 List<Path> paths = selectedUnit.cubeOnPosition.paths.Where((p) => p.path.Count <= selectedUnit.actionPointsRemain).ToList();
                 HashSet<Cube> cubesToBlink = new HashSet<Cube>();
-                foreach(var p in paths)
+                foreach (var p in paths)
                 {
-                    foreach(var c in p.path)
+                    foreach (var c in p.path)
                     {
                         cubesToBlink.Add(c);
                     }
@@ -34,7 +35,7 @@ public class UnitClickTest : MonoBehaviour
                 mapMgr.BlinkCubes(cubesToBlink.ToList(), 0.5f);
             }
 
-            else if(Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.GetMask("Cube")) && selectedUnit)
+            else if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.GetMask("Cube")) && selectedUnit)
             {
                 Path path = selectedUnit.cubeOnPosition.paths.Find((p) => p.destination == hit.transform.GetComponent<Cube>());
                 selectedUnit.stateMachine.ChangeState(new UnitRun(selectedUnit, path), StateMachine<Unit>.StateChangeMethod.PopNPush);
