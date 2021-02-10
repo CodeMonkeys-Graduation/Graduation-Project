d2cda2cf6ae95f9c1a6e21952c7dc0702d24fb3c
AdamWon <ehdgus5500@gmail.com>
2021-02-10 20:09:52 +0900

    Archer Scene 1. Archer 프리팹 만듬 2. Archer 스크립트 작성 3. Archer Animation 작업

---
+++ Assets/Scripts/Unit/Archer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : Unit
+{
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateMachine.Run();
+    }
+    protected override void SetRange()
+    {
+        basicAttackRange = new Range(new int[7, 7]{
+            {   0,0,0,1,0,0,0 },
+            {   0,0,1,1,1,0,0 },
+            {   0,1,1,1,1,1,0 },
+            {   1,1,1,1,1,1,1 },
+            {   0,1,1,1,1,1,0 },
+            {   0,0,1,1,1,0,0 },
+            {   0,0,0,1,0,0,0 }
+        });
+        basicAttackSplash = new Range(new int[1, 1] { { 1 } });
+        skillRange = null;
+        skillSplash = null;
+    }
+}
