1b836bfad72ebaa0f9f8d2b21a1ac42655ece87c
solhwi <>
2021-03-04 01:44:33 +0900

    TalkController, DialogController, UnitData, TalkData

---
+++ Assets/Scripts/DialogController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogController : MonoBehaviour
+{
+    [HideInInspector] UnitDataContainer unitDataContainer;
+
+    [Header("Set in Editor")]
+    [SerializeField] Animator illustAnim;
+
+    [SerializeField] Image illust;
+    [SerializeField] TextMeshProUGUI npcname;
+    [SerializeField] TextMeshProUGUI context;
+
+    public void SetTalk(int id, string context, int emotion)
+    {
+        illustAnim.SetTrigger("isTalk");
+
+        illust.sprite = unitDataContainer.GetSprite(id, emotion);
+        npcname.text = unitDataContainer.GetName(id);
+        this.context.text = context;
+    }
+
+}
---
+++ Assets/Scripts/TalkDataMgr.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkDataMgr
+{
+    public static TalkDataContainer LoadTalkData(int stageProgress)
+    {
+        string jsonFileName = "talkData-" + stageProgress.ToString();
+        TextAsset jsonData = Resources.Load<TextAsset>(jsonFileName);
+
+        Debug.Log("StageDataMgr: DATA LOAD COMPLETE");
+        return JsonUtility.FromJson<TalkDataContainer>(jsonData.ToString());
+    }
+}
