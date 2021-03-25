522dbb9b46ad20a2e4f80b83788b4169f80c9e68
wonAdam <ehdgus5500@gmail.com>
2021-03-25 12:17:55 +0900

    EvilMage 스킬 WaterTornado 프리팹 작성완료

---
+++ Assets/Scripts/Unit/Skills/WaterTornado.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(order = 2, fileName = "WaterTornadoSkill", menuName = "Skills/Water Tornado(EvilMage)")]
+public class WaterTornado : Skill
+{
+    public override void OnUnitSkillEnter(List<Cube> targetCubes, Cube centerCube)
+    {
+        foreach(var cube in targetCubes)
+        {
+            GameObject vfxGO = Instantiate(
+                skillVFX, cube.Platform.position, Quaternion.identity).gameObject;
+
+            Destroy(vfxGO, 3f);
+        }
+    }
+
+    public override void OnSkillAnimation(Unit ownerUnit, Unit targetUnit) => targetUnit.TakeDamage(Random.Range(amountMin, amountMax + 1), ownerUnit.transform);
+
+}
