e4deb4d971502cc0494a120bc8271875885bfdad
ehdgus5500@gmail.com <>
2021-09-10 21:24:05 +0900

    [Camera Handling] Camera를 Mouse와 WASD키로 움직임 - CameraMgr에 움직이는 로직이 있고 그러니 CameraMgr은 Battle 씬에만 생성하도록 합니다. - 우선은 "맵을 벗어나는 Camera이동의 제한"이 없으나 추후 추가할 예정.

--- Assets/Scripts/Managers/CameraMgr/CameraMgr.cs
+++ Assets/Scripts/Managers/CameraMgr/CameraMgr.cs
@@ -4,31 +4,58 @@ using UnityEngine;
 
 public class CameraMgr : SingletonBehaviour<CameraMgr>
 {
-    public Camera _camera;
+    [SerializeField] private float lerpTime = 1f; // lerpTime만에 Target으로 갑니다.
 
-    [SerializeField] float lerpTime = 1f; // lerpTime만에 Target으로 갑니다.
+    [SerializeField] private float cameraMoveSpeed = 5f;
 
-    Func<bool> unsetCondition;
-    bool unsetWhenArrivedTrigger = false;
+    private Func<bool> unsetCondition;
+    
+    private bool unsetWhenArrivedTrigger = false;
 
     private Vector3 offset;
-    public Transform target;
+   
+    private Transform target;
+    
     private float lerp = 0f;
+
+    private Vector3 screenXInWorld;
+    
+    private Vector3 screenYInWorld;
+
     void Start()
     {
-        _camera = Camera.main;
-        Ray ray = _camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-        float height = _camera.transform.position.y;
-        Vector3 posFromScreenCenter = ray.GetPoint(height / Mathf.Cos(_camera.transform.rotation.x));
-        offset = _camera.transform.position - posFromScreenCenter;
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        float height = Camera.main.transform.position.y;
+        Vector3 posFromScreenCenter = ray.GetPoint(height / Mathf.Cos(Camera.main.transform.rotation.x));
+        offset = Camera.main.transform.position - posFromScreenCenter;
+
+        Cursor.lockState = CursorLockMode.Confined;
+
+        screenXInWorld = Camera.main.transform.right;
+        screenXInWorld.y = 0f;
+        screenXInWorld.Normalize();
+        screenYInWorld = Camera.main.transform.up;
+        screenYInWorld.y = 0f;
+        screenYInWorld.Normalize();
     }
+
+    void Update()
+    {
+        
+    }
+
     void LateUpdate()
     {
         // NonTargeting
         if (target == null)
         {
             lerp = 0f;
-            return;
+
+            Vector2 direction = Vector2.zero;
+            GetMousePositionIfEdge(ref direction);
+            GeKeyInputWASD(ref direction); // 만약 WASD키가 눌리면 mouse는 override
+
+            MoveCamera(direction);
         }
 
         // Targeting
@@ -45,6 +72,57 @@ public class CameraMgr : SingletonBehaviour<CameraMgr>
         }
     }
 
+    private Vector2 GeKeyInputWASD(ref Vector2 direction)
+    {
+        // Camera Move by Mouse
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction.x = -1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            direction.x = 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction.y = -1f;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            direction.y = 1f;
+        }
+
+        return direction;
+    }
+
+    private Vector2 GetMousePositionIfEdge(ref Vector2 direction)
+    {
+        // Camera Move by Mouse
+        if (IsMousePositionLeftMost())
+        {
+            direction.x = -1f;
+        }
+        else if (IsMousePositionRightMost())
+        {
+            direction.x = 1f;
+        }
+        if (IsMousePositionBottomMost())
+        {
+            direction.y = -1f;
+        }
+        else if (IsMousePositionTopMost())
+        {
+            direction.y = 1f;
+        }
+
+        return direction;
+    }
+
+    private bool IsMousePositionLeftMost() => Mathf.Abs(Input.mousePosition.x - 0.0f) < (Screen.width / 100f);
+    private bool IsMousePositionBottomMost() => Mathf.Abs(Input.mousePosition.y - 0.0f) < (Screen.height / 100f);
+    private bool IsMousePositionRightMost() => Mathf.Abs(Input.mousePosition.x - Screen.width) < (Screen.width / 100f);
+    private bool IsMousePositionTopMost() => Mathf.Abs(Input.mousePosition.y - Screen.height) < (Screen.height / 100f);
+
     private bool CheckUnsettingTargetCondition(Vector3 targetPos)
     {
         // 도착하면 타겟을 해제하는 트리거가 set되어있고
@@ -53,7 +131,7 @@ public class CameraMgr : SingletonBehaviour<CameraMgr>
         if (unsetWhenArrivedTrigger)
         {
             Vector3 desiredCameraPos = targetPos + offset;
-            if (Vector3.Distance(_camera.transform.position, desiredCameraPos) <= Mathf.Epsilon)
+            if (Vector3.Distance(Camera.main.transform.position, desiredCameraPos) <= Mathf.Epsilon)
             {
                 unsetWhenArrivedTrigger = false;
                 return true;
@@ -77,7 +155,7 @@ public class CameraMgr : SingletonBehaviour<CameraMgr>
     private void LerpToDesiredPos(Vector3 targetPos, float lerp)
     {
         Vector3 desiredCameraPos = targetPos + offset;
-        _camera.transform.position = Vector3.Lerp(_camera.transform.position, desiredCameraPos, lerp);
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, desiredCameraPos, lerp);
     }
 
     public void SetTarget(Unit unit, bool unsetWhenArrived = false, Func<bool> unsetCondition = null)
@@ -91,9 +169,15 @@ public class CameraMgr : SingletonBehaviour<CameraMgr>
             this.unsetCondition = unsetCondition;
     }
 
-    internal void UnsetTarget()
+    public void UnsetTarget()
     {
         lerp = 0f;
         target = null;
+    }
+
+    public void MoveCamera(Vector2 screenDirection) // 화면을 기준으로 x,y
+    {
+        Vector3 worldDirection = screenDirection.x * screenXInWorld + screenDirection.y * screenYInWorld;
+        Camera.main.transform.position += Time.deltaTime * cameraMoveSpeed * (worldDirection.normalized);
     }
 }
