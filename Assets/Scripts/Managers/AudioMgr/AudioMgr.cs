c35644d75368c32884e10958b84e0bb6b2075847
ehdgus5500@gmail.com <>
2021-09-16 10:56:20 +0900

    [AudioMgr] AudioMgr 기본 구현 - Scene이 바뀌면 AudioMgr에 등록되어 있는대로 BGM을 바꿉니다. - 다른 SFX도 모두 AudioMgr을 통해 수행하도록 합니다.

---
+++ Assets/Scripts/Managers/AudioMgr/AudioMgr.cs
@@ -0,0 +1,107 @@
+﻿using RotaryHeart.Lib.SerializableDictionary;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class AudioMgr : SingletonBehaviour<AudioMgr>
+{
+    [System.Serializable]
+    public class SceneBGMDictionary : SerializableDictionaryBase<SceneMgr.Scene, AudioClip> { }
+
+    [SerializeField] private SceneBGMDictionary sceneBGMs;
+
+    [SerializeField] private AudioMixer audioMixer;
+
+    private Dictionary<AudioType, AudioMixerGroup> audioMixerGroups = new Dictionary<AudioType, AudioMixerGroup>();
+
+    private List<(AudioType type, AudioSource audioSource)> audioInstances
+        = new List<(AudioType type, AudioSource audioSource)>();
+
+    // Audio Group과 이름을 정확히 맞출 것.
+    // AudioMgr.Start참고
+    public enum AudioType
+    {
+        BGM,
+        SFX,
+        UI,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(AudioType type in Enum.GetValues(typeof(AudioType)))
+        {
+            var groups = audioMixer.FindMatchingGroups(type.ToString());
+            if(groups.Length > 0)
+            {
+                audioMixerGroups.Add(type, groups[0]);
+            }
+        }
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        UpdateBGM();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        UpdateBGM();
+    }
+
+    private void UpdateBGM()
+    {
+        var bgmAudiosTuple = audioInstances.FindAll(tuple => tuple.type == AudioType.BGM);
+        foreach(var tuple in bgmAudiosTuple)
+        {
+            audioInstances.Remove(tuple);
+            Destroy(tuple.audioSource.gameObject);
+        }
+        
+        sceneBGMs.TryGetValue(SceneMgr.Instance._currScene, out AudioClip bgmClip);
+        if(bgmClip != null)
+        {
+            PlayAudio(bgmClip, AudioType.BGM, true);
+        }
+    }
+
+    public void PlayAudio(AudioClip clip, AudioType type, bool loop)
+    {
+        GameObject audioGO = new GameObject(type.ToString());
+        DontDestroyOnLoad(audioGO);
+        AudioSource audioSource = audioGO.AddComponent<AudioSource>();
+
+        audioSource.playOnAwake = true;
+        audioSource.loop = loop;
+        audioSource.clip = clip;
+        if (audioMixerGroups.TryGetValue(type, out AudioMixerGroup group))
+            audioSource.outputAudioMixerGroup = group;
+
+        audioInstances.Add((type, audioSource));
+
+        audioSource.Play();
+
+        if (!loop)
+        {
+            StartCoroutine(DestroyOnEnd(audioGO, clip.length));
+        }
+    }
+
+    private IEnumerator DestroyOnEnd(GameObject audioGO, float seconds)
+    {
+        yield return new WaitForSeconds(seconds + 0.5f);
+
+        if(audioGO != null)
+        {
+            audioInstances.RemoveAll((tuple) => tuple.audioSource.gameObject == audioGO);
+            Destroy(audioGO);
+        }
+    }
+}
--- Assets/Scripts/Managers/SceneMgr/SceneMgr.cs
+++ Assets/Scripts/Managers/SceneMgr/SceneMgr.cs
@@ -9,6 +9,7 @@ public class SceneMgr : SingletonBehaviour<SceneMgr>
 {
     [System.Serializable]
     public enum Scene // ToString()으로 검사하기때문에 Scene의 이름과 정확히 일치해야합니다. 
+        // 그리고 이미 있는 중간에 새로운 enum을 넣지말고 아래에 이어서 넣습니다. (중간에 넣으면 설정한 것들이 깨져버림...)
     { 
         Main, 
         Dialog1,
@@ -28,8 +29,21 @@ public class SceneMgr : SingletonBehaviour<SceneMgr>
         { Scene.Battle3.ToString(), Scene.Battle3 },
         { Scene.Battle4.ToString(), Scene.Battle4 },
     };
+
+    [SerializeField/*DEBUG*/] public Scene _currScene;
+
+    private void Awake()
+    {
+        base.Awake();
+
+        // AudioMgr이 Start에서 _currScene을 접근하기때문에 Awake에서 수행해야함.
+        sceneMap.TryGetValue(SceneManager.GetActiveScene().name, out Scene currScene);
+        _currScene = currScene;
+    }
+
     public void LoadScene(Scene sceneEnum, Action<float> onSceneLoad = null)
     {
+        _currScene = sceneEnum;
         StartCoroutine(OnLoadSceneCoroutine(sceneEnum, onSceneLoad));
     }
     private IEnumerator OnLoadSceneCoroutine(Scene SceneEnum, Action<float> onSceneLoad)
